6d158e870e3cf5dbf438ad2bde2c9134aebbcf6c
GabrielCrespo <gabriel.crespo1942@gmail.com>
2021-12-01 23:00:46 -0300

    Customização de tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
+import AppError from "@shared/errors/AppError";
 
 const port = 3333;
 
@@ -10,6 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: error.statusCode,
+            message: error.message,
+        });
+    }
+    return response.status(500).json({
+        status: "erro",
+        message: "Erro interno do servidor!"
+    })
+})
 
 app.listen(port, () => {
     console.log(`O servidor está executando na porta ${port}`);
