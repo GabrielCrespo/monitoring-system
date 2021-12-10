5a0e78783ed5f54e4a650d918e32710bd24214e6
GabrielCrespo <gabriel.crespo1942@gmail.com>
2021-12-10 00:40:26 -0300

    Adicionando validação a criação de um curso

--- src/modules/course/routes/course.routes.ts
+++ src/modules/course/routes/course.routes.ts
@@ -1,10 +1,18 @@
-import routes from "@shared/http/routes";
 import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
 import CourseController from "../controllers/CourseController";
 
 const courseRouter = Router();
 const courseController = new CourseController();
 
-courseRouter.post("/", courseController.create);
+courseRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      descricao: Joi.string().required(),
+    },
+  }),
+  courseController.create
+);
 
 export default courseRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
 import "@shared/typeorm";
+import { errors } from "celebrate";
 
 const port = 3333;
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
