5e59165aa05cee2ecae2529d53e8e8b21884b55c
GabrielCrespo <gabriel.crespo1942@gmail.com>
2021-12-11 21:31:43 -0300

    Inclusão da rota de funções no arquivo principal de rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import courseRouter from "@modules/course/routes/course.routes";
+import rolesRouter from "@modules/roles/routes/roles.routes";
 
 const routes = Router();
 
 routes.use("/course", courseRouter);
+routes.use("/roles", rolesRouter);
 
 
 export default routes;
