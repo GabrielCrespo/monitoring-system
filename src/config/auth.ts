5f43220e1e35e50611d45c58a9706eecef3853bc
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-01-07 00:08:03 -0300

    Geração do token de autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: "8fc1d1df9b55d8066dff87e861920c2e",
+        expiresIn: "1d",
+    }
+}
--- src/modules/teacher/services/CreateTeacherSessionService.ts
+++ src/modules/teacher/services/CreateTeacherSessionService.ts
@@ -2,19 +2,22 @@ import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import Teacher from "../typeorm/entities/Teacher";
 import TeacherRepository from "../typeorm/repositories/TeacherRepository";
-import bcrypt, { compare } from "bcrypt";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import auth from "@config/auth";
 
 interface IRequest {
   email: string;
   senha: string;
 }
 
-// interface IResponse {
-//   professor: Teacher;
-// }
+interface IResponse {
+  professor: Teacher;
+  token: string;
+}
 
 class CreateTeacherSessionService {
-  public async execute({ email, senha }: IRequest): Promise<Teacher> {
+  public async execute({ email, senha }: IRequest): Promise<IResponse> {
     const teacherRepository = getCustomRepository(TeacherRepository);
 
     const teacher = await teacherRepository.findByEmail(email);
@@ -26,7 +29,7 @@ class CreateTeacherSessionService {
       );
     }
 
-    const confirmedPassword = await compare(senha, teacher.senha);
+    const confirmedPassword = await bcrypt.compare(senha, teacher.senha);
 
     if (!confirmedPassword) {
       throw new AppError(
@@ -35,7 +38,15 @@ class CreateTeacherSessionService {
       );
     }
 
-    return teacher;
+    const token = sign({}, auth.jwt.secret, {
+      subject: teacher.id.toString(),
+      expiresIn: auth.jwt.expiresIn,
+    })
+
+    return {
+      professor: teacher,
+      token
+    };
   }
 }
 
