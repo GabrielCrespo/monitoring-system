b8d506a562ce5cf80e1ee5140bc0b22bc8c9b62f
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-01-10 21:58:08 -0300

    Adicionando validações no serviço de atualização de turma

--- src/modules/class/services/UpdateTeamService.ts
+++ src/modules/class/services/UpdateTeamService.ts
@@ -1,4 +1,5 @@
 import DayOfWeek from "@modules/dayofweek/typeorm/entities/DayOfWeek";
+import DayOfWeekRepository from "@modules/dayofweek/typeorm/repositories/DayOfWeekRepository";
 import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import Team from "../typeorm/entities/Team";
@@ -21,6 +22,7 @@ class UpdateTeamService {
     dias_da_semana,
   }: IRequest): Promise<Team> {
     const teamRepository = getCustomRepository(TeamRepository);
+    const dayofweekRepository = getCustomRepository(DayOfWeekRepository);
 
     const team = await teamRepository.findOne(id);
 
@@ -32,6 +34,27 @@ class UpdateTeamService {
 
     if (descricaoExists && team.descricao !== descricao) {
       throw new AppError("A turma já está cadastrada!");
+    }
+
+    
+    const existsDayOfTheWeek = await dayofweekRepository.findAllByIds(
+      dias_da_semana
+    );
+
+    if (!existsDayOfTheWeek.length) {
+      throw new AppError("Não foi possível encontrar os dias da semana!");
+    }
+
+    const existsDayOfTheWeekIds = existsDayOfTheWeek.map(
+      (dayOfWeek) => dayOfWeek.id
+    );
+
+    const checkInexistentDayOfWeek = dias_da_semana.filter(
+      (dayOfWeek) => !existsDayOfTheWeekIds.includes(dayOfWeek.id)
+    );
+
+    if (checkInexistentDayOfWeek.length) {
+      throw new AppError("Não foi possível encontrar algum dos dias da semana!");
     }
 
     team.descricao = descricao;
