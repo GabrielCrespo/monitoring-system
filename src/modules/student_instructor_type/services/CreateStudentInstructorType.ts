ed53f61c42e7cb49ae13301bf6b6f40653196ed1
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-02-03 17:27:46 -0300

    Serviço de criação aluno instrutor tipo

---
+++ src/modules/student_instructor_type/services/CreateStudentInstructorType.ts
@@ -0,0 +1,35 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import StudentInstructorType from "../typeorm/entities/StudentInstructorType";
+import StudentInstructorTypeRepository from "../typeorm/repositories/StudentInstructorTypeRepository";
+
+interface IRequest {
+  descricao: string;
+}
+
+class CreateStudentInstructorType {
+  public async execute({
+    descricao,
+  }: IRequest): Promise<StudentInstructorType> {
+    const studentInstructorTypeRepository = getCustomRepository(
+      StudentInstructorTypeRepository
+    );
+
+    const studentInstructorTypeExists =
+      await studentInstructorTypeRepository.findByDescription(descricao);
+
+    if (studentInstructorTypeExists) {
+      throw new AppError("Esse tipo de aluno instrutor já está cadastrado");
+    }
+
+    const studentInstructorType = studentInstructorTypeRepository.create({
+      descricao,
+    });
+
+    await studentInstructorTypeRepository.save(studentInstructorType);
+
+    return studentInstructorType;
+  }
+}
+
+export default CreateStudentInstructorType;
