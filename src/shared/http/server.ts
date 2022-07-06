a229163ce65df5da48b5d1981e2b5e38dbaf9a9b
GabrielCrespo <gabriel.crespo1942@gmail.com>
2022-07-05 22:59:09 -0300

    Rota estática

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,36 +6,37 @@ import routes from "./routes";
 import AppError from "@shared/errors/AppError";
 import "@shared/typeorm";
 import { errors } from "celebrate";
-import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
+import uploadConfig from "@config/upload";
+// import swaggerUI from "swagger-ui-express";
+// import swaggerJsDoc from "swagger-jsdoc";
 
 const port = 3333;
 
-const options = {
-  definition: {
-    swagger: "2.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "Monitoring system",
-    },
-    servers: [
-      {
-        url: "http://localhost:3080",
-      },
-    ],
-  },
-  apis: ["**/*.{ts,js"],
-};
+// const options = {
+//   definition: {
+//     swagger: "2.0",
+//     info: {
+//       title: "Library API",
+//       version: "1.0.0",
+//       description: "Monitoring system",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3080",
+//       },
+//     ],
+//   },
+//   apis: ["**/*.{ts,js"],
+// };
 
-const specs = swaggerJsDoc(options);
+// const specs = swaggerJsDoc(options);
 
 const app = express();
-app.use("/api", swaggerUI.serve, swaggerUI.setup(specs));
+// app.use("/api", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
